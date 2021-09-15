8abe8ac0640685f08576913f00793444ae1c149a
Jacob Ross <jacobkarmelross@gmail.com>
2021-09-15 21:04:34 +0200

    added cube,rotation, and orbit controls

--- start/lecture2_1/app.js
+++ start/lecture2_1/app.js
@@ -1,22 +1,40 @@
-import * as THREE from '../../libs/three/three.module.js';
+import * as THREE from '../../libs/three/three.module.js'
 import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
 
 class App {
 	constructor() {
+		console.log('constructing')
 		const container = document.createElement('div');
 		document.body.appendChild(container);
 
-		this.camera = new THREE / THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100)
+		this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100)
 		this.camera.position.set(0, 0, 4);
 
-		this.scene = new Three.Scene();
+		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color(0xaaaaaa)
+
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3)
+		this.scene.add(ambient)
+
+		const light = new THREE.DirectionalLight()
+		light.position.set(0.2, 1, 1)
+		this.scene.add(light)
+
 		this.renderer = new THREE.WebGLRenderer({ antialias: true })
 		this.renderer.setPixelRatio(window.devicePixelRatio)
 		this.renderer.setSize(window.innerWidth, window.innerHeight)
 		container.appendChild(this.renderer.domElement)
 
 		this.renderer.setAnimationLoop(this.render.bind(this))
+
+		const geometry = new THREE.BoxBufferGeometry();
+		const material = new THREE.MeshStandardMaterial({ color: 0xff0000 })
+
+		this.mesh = new THREE.Mesh(geometry, material)
+
+		this.scene.add(this.mesh)
+
+		const controls = new OrbitControls(this.camera, this.renderer.domElement)
 
 
 		window.addEventListener('resize', this.resize.bind(this));
@@ -27,6 +45,7 @@ class App {
 	}
 
 	render() {
+		this.mesh.rotateX(0.01)
 		this.renderer.render(this.scene, this.camera)
 	}
 }
